abb2daa747743da3fe8cbb6d565b305b909f3134
Rebecado231 <rebeca.cado@gmail.com>
2025-08-12 09:17:45 +0200

    últimos cambios

--- chats/agent_chat.py
+++ chats/agent_chat.py
@@ -2,25 +2,21 @@ from os import getenv
 from datetime import datetime
 
 from langchain.chat_models import init_chat_model
-from langchain.tools import tool
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import InMemorySaver
 
 API_KEY = getenv("OPENAI_API_KEY")
 
 
-@tool
 def get_current_date() -> str:
     """Función para obtener la fecha actual."""
     return datetime.now().date().isoformat()
 
 
-memory = InMemorySaver()
-
-
 def create_agent():
     """Función para crear un agente de LangChain."""
     model = init_chat_model("openai:gpt-4.1", temperature=0.5, timeout=5)
+    memory = InMemorySaver()
     agente = create_react_agent(
         model=model,
         tools=[get_current_date],
