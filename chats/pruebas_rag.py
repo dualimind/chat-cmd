b47052022890788ffeaf13b3b6e88054db84ed81
Rebecado231 <rebeca.cado@gmail.com>
2025-08-14 10:44:14 +0200

    primera parte de RAG

---
+++ chats/pruebas_rag.py
@@ -0,0 +1,74 @@
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chat_models import init_chat_model
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain import hub
+from langchain_core.documents import Document
+from typing_extensions import List, TypedDict
+from langgraph.graph import START, StateGraph
+import requests
+
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+vector_store = InMemoryVectorStore(embeddings)
+
+
+bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs={"parse_only": bs4_strainer},
+)
+docs = loader.load()
+assert len(docs) == 1
+print(f"Total characters: {len(docs[0].page_content)}")
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    add_start_index=True,
+)
+all_splits = text_splitter.split_documents(docs)
+print(f"Split blog post into {len(all_splits)} sub-documents.")
+
+document_ids = vector_store.add_documents(documents=all_splits)
+print(document_ids[:])
+
+prompt = hub.pull("rlm/rag-prompt")
+
+example_messages = prompt.invoke(
+    {"context": "(context goes here)", "question": "(question goes here)"}
+).to_messages()
+
+assert len(example_messages) == 1
+print(example_messages[0].content)
+
+
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+result = graph.invoke({"question": "What is Task Decomposition?"})  # type: ignore
+
+print(f"Context: {result['context']}\n\n")
+print(f"Answer: {result['answer']}")
